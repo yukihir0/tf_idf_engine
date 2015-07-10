3656076a70587185c4e224975ecacbb0cd2533d0
yukihir0 <yukihiro.cotori@gmail.com>
2015-07-10 10:11:14 +0900

    modify tabspace 4 -> 2

--- lib/tf_idf_engine.rb
+++ lib/tf_idf_engine.rb
@@ -3,60 +3,60 @@ require 'tf_idf_engine/version'
 require 'tf_idf_engine/calculator/tf_idf_calculator'
 
 class TfIdfEngine
-    include TfIdfEngineCalculator
+  include TfIdfEngineCalculator
 
-    NIL_ID_ERROR          = 'nil id is  inputted.'
-    NIL_DOC_ERROR         = 'nil doc is inputted.'
-    NOT_ARRAY_DOC_ERROR   = 'doc that is not an instance of array is inputted.'
-    ALREADY_USED_ID_ERROR = 'already used id is inputted.'
-    UNUSED_ID_ERROR       = 'unused id is inputted.'
+  NIL_ID_ERROR          = 'nil id is  inputted.'
+  NIL_DOC_ERROR         = 'nil doc is inputted.'
+  NOT_ARRAY_DOC_ERROR   = 'doc that is not an instance of array is inputted.'
+  ALREADY_USED_ID_ERROR = 'already used id is inputted.'
+  UNUSED_ID_ERROR       = 'unused id is inputted.'
 
-    public
-    def initialize
-        @t_o        = Hash.new
-        @calculator = TfIdfCalculator.instance
-    end
+  public
+  def initialize
+    @t_o        = Hash.new
+    @calculator = TfIdfCalculator.instance
+  end
 
-    def input(id, doc)
-        raise NIL_ID_ERROR if nil_or_empty?(id)
-        raise NIL_DOC_ERROR if nil_or_empty?(doc)
-        raise NOT_ARRAY_DOC_ERROR unless doc.instance_of?(Array)
-        raise ALREADY_USED_ID_ERROR if used?(id)
+  def input(id, doc)
+    raise NIL_ID_ERROR if nil_or_empty?(id)
+    raise NIL_DOC_ERROR if nil_or_empty?(doc)
+    raise NOT_ARRAY_DOC_ERROR unless doc.instance_of?(Array)
+    raise ALREADY_USED_ID_ERROR if used?(id)
 
-        @t_o[id] = @calculator.term_occurrence(doc)
-    end
+    @t_o[id] = @calculator.term_occurrence(doc)
+  end
 
-    def analyze_all
-        tf  = @calculator.term_frequency_all(@t_o)
-        idf = @calculator.inverse_document_frequency(@t_o)
-        @calculator.tf_idf(tf, idf)
-    end
-  
-    def analyze(doc)
-        raise NIL_DOC_ERROR if nil_or_empty?(doc)
-        raise NOT_ARRAY_DOC_ERROR unless doc.instance_of?(Array)
+  def analyze_all
+    tf  = @calculator.term_frequency_all(@t_o)
+    idf = @calculator.inverse_document_frequency(@t_o)
+    @calculator.tf_idf(tf, idf)
+  end
 
-        # input tempolary cod
-        id = 'ANALYZE_ONCE_ID'
-        input(id, doc)
-        
-        tf     = {id => @calculator.term_frequency(@t_o[id])}
-        idf    = @calculator.inverse_document_frequency(@t_o)
-        tf_idf = @calculator.tf_idf(tf, idf)
-        result = tf_idf[id]
-        
-        # delete tempolary doc
-        @t_o.delete(id)
+  def analyze(doc)
+    raise NIL_DOC_ERROR if nil_or_empty?(doc)
+    raise NOT_ARRAY_DOC_ERROR unless doc.instance_of?(Array)
 
-        result
-    end
+    # input tempolary cod
+    id = 'ANALYZE_ONCE_ID'
+    input(id, doc)
 
-    private
-    def nil_or_empty?(target)
-        target.nil? || target.empty?
-    end
+    tf     = {id => @calculator.term_frequency(@t_o[id])}
+    idf    = @calculator.inverse_document_frequency(@t_o)
+    tf_idf = @calculator.tf_idf(tf, idf)
+    result = tf_idf[id]
 
-    def used?(id)
-        @t_o.keys.include?(id)
-    end
+    # delete tempolary doc
+    @t_o.delete(id)
+
+    result
+  end
+
+  private
+  def nil_or_empty?(target)
+    target.nil? || target.empty?
+  end
+
+  def used?(id)
+    @t_o.keys.include?(id)
+  end
 end
--- spec/tf_idf_engine_spec.rb
+++ spec/tf_idf_engine_spec.rb
@@ -2,274 +2,274 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe TfIdfEngine do
-    context 'uninitialized' do
-        describe '#initialize' do
-            context 'when call' do
-                it 'initialized' do
-                    engine     = TfIdfEngine.new
-                    t_o        = engine.instance_eval('@t_o')
-                    calculator = engine.instance_eval('@calculator')
+  context 'uninitialized' do
+    describe '#initialize' do
+      context 'when call' do
+        it 'initialized' do
+          engine     = TfIdfEngine.new
+          t_o        = engine.instance_eval('@t_o')
+          calculator = engine.instance_eval('@calculator')
 
-                    t_o.should == {}
-                    calculator.instance_of?(TfIdfCalculator).should be_truthy
-                end
-            end
+          t_o.should == {}
+          calculator.instance_of?(TfIdfCalculator).should be_truthy
         end
+      end
+    end
+  end
+
+  context 'have no doc' do
+    before(:each) do
+      @engine = TfIdfEngine.new
+      @id1    = 'id_001'
+      @doc1   = %w(a b)
     end
 
-    context 'have no doc' do
-        before(:each) do
-            @engine = TfIdfEngine.new
-            @id1    = 'id_001'
-            @doc1   = %w(a b)
+    describe '#input' do
+      context 'when nil id, nil doc input' do
+        it 'raise error' do
+          expect { @engine.input(nil, nil)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
         end
+      end
 
-        describe '#input' do
-            context 'when nil id, nil doc input' do
-                it 'raise error' do
-                    expect { @engine.input(nil, nil)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
-                end
-            end
-
-            context 'when nil id input' do
-                 it 'raise error' do
-                    expect { @engine.input(nil, @doc1)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
-                end
-            end
-
-            context 'when empty id input' do
-                 it 'raise error' do
-                    expect { @engine.input('', @doc1)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
-                end
-            end
- 
-            context 'when nil doc input' do
-                 it 'raise error' do
-                    expect { @engine.input(@id1, nil)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
-                end
-            end
- 
-            context 'when empty doc input' do
-                 it 'raise error' do
-                    expect { @engine.input(@id1, '')
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
-                end
-            end
-            
-            context 'when not array doc input' do
-                 it 'raise error' do
-                    expect { @engine.input(@id1, 'doc')
-                    }.to raise_error(RuntimeError, TfIdfEngine::NOT_ARRAY_DOC_ERROR)
-                end
-            end
-
-            context 'when id, doc input' do
-                it 't_o' do
-                    expected = {"id_001" => {"a" => 1, "b" => 1}}
-
-                    @engine.input(@id1, @doc1)
-                    t_o = @engine.instance_eval('@t_o')
-                    t_o.should == expected
-                end
-            end
+      context 'when nil id input' do
+        it 'raise error' do
+          expect { @engine.input(nil, @doc1)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
         end
+      end
 
-        describe '#analyze_all' do
-            context 'when call' do
-                it 'tf_idf' do
-                    tf_idf = @engine.analyze_all
-                    tf_idf.should == {}
-                end
-            end
+      context 'when empty id input' do
+        it 'raise error' do
+          expect { @engine.input('', @doc1)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
         end
+      end
 
-        describe '#analyze' do
-            context 'when nil doc input' do
-                it 'raise error' do
-                    expect { @engine.analyze(nil)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
-                end
-            end
-
-            context 'when empty doc input' do
-                it 'raise error' do
-                    expect { @engine.analyze('')
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
-                end
-            end
-
-            context 'when doc input' do
-                it 'tf_idf' do
-                    expected1 = {"a" => 0.0, "b" => 0.0}
-                    tf_idf_once = @engine.analyze(@doc1)
-                    tf_idf_once.should == expected1
-
-                    expected2 = {}
-                    tf_idf = @engine.analyze_all
-                    tf_idf.should == expected2
-                end
-            end
+      context 'when nil doc input' do
+        it 'raise error' do
+          expect { @engine.input(@id1, nil)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
         end
+      end
 
-        describe '#used?' do
-            context 'when nil id input' do
-                it 'false' do
-                    @engine.send(:used?, nil).should be_falsey
-                end
-            end
-
-            context 'when empty id input' do
-                it 'false ' do
-                    @engine.send(:used?,'').should be_falsey
-                end
-            end
-
-            context 'when id_001 input' do
-                it 'false' do
-                    @engine.send(:used?, @id1).should be_falsey
-                end
-            end
+      context 'when empty doc input' do
+        it 'raise error' do
+          expect { @engine.input(@id1, '')
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
         end
+      end
+
+      context 'when not array doc input' do
+        it 'raise error' do
+          expect { @engine.input(@id1, 'doc')
+          }.to raise_error(RuntimeError, TfIdfEngine::NOT_ARRAY_DOC_ERROR)
+        end
+      end
+
+      context 'when id, doc input' do
+        it 't_o' do
+          expected = {"id_001" => {"a" => 1, "b" => 1}}
+
+          @engine.input(@id1, @doc1)
+          t_o = @engine.instance_eval('@t_o')
+          t_o.should == expected
+        end
+      end
     end
 
-    context 'have 1 doc' do
-        before(:each) do
-            @engine = TfIdfEngine.new
-            @id1    = 'id_001'
-            @doc1   = %w(a b)
-            @id2    = 'id_002'
-            @doc2   = %w(b c)
-            @engine.input(@id1, @doc1)
+    describe '#analyze_all' do
+      context 'when call' do
+        it 'tf_idf' do
+          tf_idf = @engine.analyze_all
+          tf_idf.should == {}
         end
-
-        describe '#input' do
-            context 'when nil id, nil doc input' do
-                it 'raise error' do
-                    expect { @engine.input(nil, nil)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
-                end
-            end
-
-            context 'when nil id input' do
-                 it 'raise error' do
-                    expect { @engine.input(nil, @doc1)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
-                end
-            end
-
-            context 'when empty id input' do
-                 it 'raise error' do
-                    expect { @engine.input('', @doc1)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
-                end
-            end
- 
-            context 'when nil doc input' do
-                 it 'raise error' do
-                    expect { @engine.input(@id1, nil)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
-                end
-            end
-            
-            context 'when empty doc input' do
-                 it 'raise error' do
-                    expect { @engine.input(@id1, '')
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
-                end
-            end
-
-            context 'when not array doc input' do
-                 it 'raise error' do
-                    expect { @engine.input(@id1, 'doc')
-                    }.to raise_error(RuntimeError, TfIdfEngine::NOT_ARRAY_DOC_ERROR)
-                end
-            end
-
-            context 'when id_001 input' do
-                it 'raise error' do
-                    expect { @engine.input(@id1, @doc1)
-                    }.to raise_error(RuntimeError, TfIdfEngine::ALREADY_USED_ID_ERROR)
-                end
-            end
-
-            context 'when new id input' do
-                it 't_o' do
-                    expected = {"id_001" => {"a" => 1, "b" => 1},
-                                "id_002" => {"b" => 1, "c" => 1}}
-
-                    @engine.input(@id2, @doc2)
-                    t_o = @engine.instance_eval('@t_o').should == expected
-                end
-            end
-        end
-
-        describe '#analyze_all' do
-            context 'when call' do
-                it 'tf_idf' do
-                    expected = {'id_001' => {'a' => 0.0, 'b' => 0.0}}
-
-                    tf_idf = @engine.analyze_all
-                    tf_idf.should == expected
-                end
-            end
-        end
-
-        describe '#analyze' do
-            context 'when nil doc input' do
-                it 'raise error' do
-                    expect { @engine.analyze(nil)
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
-                end
-            end
-
-            context 'when empty doc input' do
-                it 'raise error' do
-                    expect { @engine.analyze('')
-                    }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
-                end
-            end
-
-            context 'when doc input' do
-                it 'tf_idf' do
-                    expected1 = {"b" => 0.0, "c" => 0.1505149978319906}
-                    tf_idf_once = @engine.analyze(@doc2)
-                    tf_idf_once.should == expected1
-
-                    expected2 = {"id_001"=>{"a"=>0.0, "b"=>0.0}}
-                    tf_idf = @engine.analyze_all
-                    tf_idf.should == expected2
-                end
-            end
-        end
-
-        describe '#used?' do
-            context 'when nil id input' do
-                it 'false' do
-                    @engine.send(:used?, nil).should be_falsey
-                end
-            end
-
-            context 'when empty id input' do
-                it 'false ' do
-                    @engine.send(:used?, '').should be_falsey
-                end
-            end
-
-            context 'when id_001 input' do
-                it 'true' do
-                    @engine.send(:used?, @id1).should be_truthy
-                end
-            end
-
-            context 'when id_002 input' do
-                it 'false' do
-                    @engine.send(:used?, @id2).should be_falsey
-                end
-            end
-        end
+      end
     end
+
+    describe '#analyze' do
+      context 'when nil doc input' do
+        it 'raise error' do
+          expect { @engine.analyze(nil)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
+        end
+      end
+
+      context 'when empty doc input' do
+        it 'raise error' do
+          expect { @engine.analyze('')
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
+        end
+      end
+
+      context 'when doc input' do
+        it 'tf_idf' do
+          expected1 = {"a" => 0.0, "b" => 0.0}
+          tf_idf_once = @engine.analyze(@doc1)
+          tf_idf_once.should == expected1
+
+          expected2 = {}
+          tf_idf = @engine.analyze_all
+          tf_idf.should == expected2
+        end
+      end
+    end
+
+    describe '#used?' do
+      context 'when nil id input' do
+        it 'false' do
+          @engine.send(:used?, nil).should be_falsey
+        end
+      end
+
+      context 'when empty id input' do
+        it 'false ' do
+          @engine.send(:used?,'').should be_falsey
+        end
+      end
+
+      context 'when id_001 input' do
+        it 'false' do
+          @engine.send(:used?, @id1).should be_falsey
+        end
+      end
+    end
+  end
+
+  context 'have 1 doc' do
+    before(:each) do
+      @engine = TfIdfEngine.new
+      @id1    = 'id_001'
+      @doc1   = %w(a b)
+      @id2    = 'id_002'
+      @doc2   = %w(b c)
+      @engine.input(@id1, @doc1)
+    end
+
+    describe '#input' do
+      context 'when nil id, nil doc input' do
+        it 'raise error' do
+          expect { @engine.input(nil, nil)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
+        end
+      end
+
+      context 'when nil id input' do
+        it 'raise error' do
+          expect { @engine.input(nil, @doc1)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
+        end
+      end
+
+      context 'when empty id input' do
+        it 'raise error' do
+          expect { @engine.input('', @doc1)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_ID_ERROR)
+        end
+      end
+
+      context 'when nil doc input' do
+        it 'raise error' do
+          expect { @engine.input(@id1, nil)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
+        end
+      end
+
+      context 'when empty doc input' do
+        it 'raise error' do
+          expect { @engine.input(@id1, '')
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
+        end
+      end
+
+      context 'when not array doc input' do
+        it 'raise error' do
+          expect { @engine.input(@id1, 'doc')
+          }.to raise_error(RuntimeError, TfIdfEngine::NOT_ARRAY_DOC_ERROR)
+        end
+      end
+
+      context 'when id_001 input' do
+        it 'raise error' do
+          expect { @engine.input(@id1, @doc1)
+          }.to raise_error(RuntimeError, TfIdfEngine::ALREADY_USED_ID_ERROR)
+        end
+      end
+
+      context 'when new id input' do
+        it 't_o' do
+          expected = {"id_001" => {"a" => 1, "b" => 1},
+                      "id_002" => {"b" => 1, "c" => 1}}
+
+          @engine.input(@id2, @doc2)
+          t_o = @engine.instance_eval('@t_o').should == expected
+        end
+      end
+    end
+
+    describe '#analyze_all' do
+      context 'when call' do
+        it 'tf_idf' do
+          expected = {'id_001' => {'a' => 0.0, 'b' => 0.0}}
+
+          tf_idf = @engine.analyze_all
+          tf_idf.should == expected
+        end
+      end
+    end
+
+    describe '#analyze' do
+      context 'when nil doc input' do
+        it 'raise error' do
+          expect { @engine.analyze(nil)
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
+        end
+      end
+
+      context 'when empty doc input' do
+        it 'raise error' do
+          expect { @engine.analyze('')
+          }.to raise_error(RuntimeError, TfIdfEngine::NIL_DOC_ERROR)
+        end
+      end
+
+      context 'when doc input' do
+        it 'tf_idf' do
+          expected1 = {"b" => 0.0, "c" => 0.1505149978319906}
+          tf_idf_once = @engine.analyze(@doc2)
+          tf_idf_once.should == expected1
+
+          expected2 = {"id_001"=>{"a"=>0.0, "b"=>0.0}}
+          tf_idf = @engine.analyze_all
+          tf_idf.should == expected2
+        end
+      end
+    end
+
+    describe '#used?' do
+      context 'when nil id input' do
+        it 'false' do
+          @engine.send(:used?, nil).should be_falsey
+        end
+      end
+
+      context 'when empty id input' do
+        it 'false ' do
+          @engine.send(:used?, '').should be_falsey
+        end
+      end
+
+      context 'when id_001 input' do
+        it 'true' do
+          @engine.send(:used?, @id1).should be_truthy
+        end
+      end
+
+      context 'when id_002 input' do
+        it 'false' do
+          @engine.send(:used?, @id2).should be_falsey
+        end
+      end
+    end
+  end
 end
